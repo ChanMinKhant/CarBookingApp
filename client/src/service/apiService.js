fb68beea193eafb7d7740675fbbcc793f5c18c83
Chan Min Khant <jiyonfire@gmail.com>
2024-03-09 22:10:34 +0630

    Refactor code for consistency and readability

--- client/src/service/apiService.js
+++ client/src/service/apiService.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-const apiService = axios.create({
-  baseURL: 'http://localhost:3000',
-  withCredentials: true,
-});
-
 // const apiService = axios.create({
-//   baseURL: 'https://carbookingapp.onrender.com',
+//   baseURL: 'http://localhost:3000',
 //   withCredentials: true,
 // });
+
+const apiService = axios.create({
+  baseURL: 'https://carbookingapp.onrender.com',
+  withCredentials: true,
+});
 
 export default apiService;
 
